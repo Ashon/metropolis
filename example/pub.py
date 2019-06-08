e61b7b4edc9a37720c305e43a4459444209028d0
ashon <ashon8813@gmail.com>
2019-06-08 22:10:58 +0900

    Use sanic for nats http-proxy

--- example/pub.py
+++ example/pub.py
@@ -2,11 +2,9 @@ import os
 import sys
 sys.path.append(os.getcwd()) # noqa E402
 
-import random
 import time
 
 from nats_worker.core.worker import Worker
-from nats_worker.core.utils import simple_eventloop
 import settings
 
 
@@ -14,10 +12,10 @@ def main(name):
     worker = Worker(settings)
     payload = b''
 
-    async def publish_random_msg(worker, loop):
+    async def publish_random_msg(worker):
         counter = 0
         now = time.perf_counter()
-        async with worker.nats_driver(loop) as nats:
+        async with worker.nats_driver() as nats:
             while 1:
                 _ = await nats.request(name, payload)
                 counter += 1
@@ -26,10 +24,9 @@ def main(name):
                     print(f'{100 / elapsed:.3f} RPS')
                     now = time.perf_counter()
 
-    with simple_eventloop() as loop:
-        loop.run_until_complete(
-            publish_random_msg(worker, loop)
-        )
+    worker.run_until_complete(
+        publish_random_msg(worker)
+    )
 
 
 if __name__ == "__main__":
