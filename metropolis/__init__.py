c4302f302ece775740297ebdeff23fec7c7a9bb6
ashon <ashon8813@gmail.com>
2019-11-08 23:41:56 +0900

    Fix typo

--- metropolis/__init__.py
+++ metropolis/__init__.py
@@ -1,4 +1,4 @@
-from worker import Worker
-from gateway import Gateway
+from metropolis.worker import Worker
+from metropolis.gateway import Gateway
 
 __all__ = ['Worker', 'Gateway']
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="metropolis",
-    version="0.1.1",
+    version="0.1.2",
     license='MIT',
     author="ashon lee",
     author_email="ashon8813@gmail.com",
