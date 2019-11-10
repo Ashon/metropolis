6e0657e620a41fb279c6ceb0516a839a6f618e42
ashon <ashon8813@gmail.com>
2019-11-10 14:31:47 +0900

    Bump up version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="metropolis",
-    version="0.1.2",
+    version="0.1.3",
     license='MIT',
     author="ashon lee",
     author_email="ashon8813@gmail.com",
