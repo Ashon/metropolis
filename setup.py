7cfaed68f4d5d79e5d88875ddaa8c69ec070ef73
ashon.lee(이주원) <ashon.lee@kakaocorp.com>
2019-12-02 16:15:43 +0900

    tag 0.1.4.dev3

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="metropolis",
-    version="0.1.4.dev2",
+    version="0.1.4.dev3",
     license='MIT',
     author="ashon lee",
     author_email="ashon8813@gmail.com",
